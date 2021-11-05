2e8bd3b52abc3e059fdd8455ad22136a4b1f3d38
justDenzil <vikash.denzil@gmail.com>
2021-11-05 14:04:52 +0530

    added init. icons and animated navBar to minimize itself

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FloatingMenu.css'
 import {ReactComponent as UploadLogoImage} from '../assets/Upload_Logo_SVG.svg'
+import {ReactComponent as BackButton} from '../assets/icons/BackButton.svg'
+import {ReactComponent as FrontButton} from '../assets/icons/FrontButton.svg'
 
-function UploadLogo() {
+    
+
+
+function FloatingMenu() {
+
+    const [inactive, setInactive] = useState(false);
+
     return (
+        <div className={`menu-body ${inactive ? "inactive" : ""}`}>
+            <div className="toggle-menu-btn" onClick={() => {setInactive(!inactive);}}>
+            {inactive ? (
+            <FrontButton className="back-btn" />
+          ) : (
+            <BackButton className="back-btn" />
+          )}
+            
+        </div>
         <div className="upload-logo-container">
             <div className="upload-logo-image">
                 <UploadLogoImage/>
             </div>
             <div className="upload-logo-text">Upload logo</div>
         </div>
-    )
-}
-
-function FloatingMenu() {
-    return (
-        <div className="menu-body">
-            <UploadLogo className="upload-logo"/>
+        <div className="divider">
+        </div>
         </div>
     )
 }
